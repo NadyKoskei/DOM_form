103c14f26448a46e19a43ed900ce1706a4915728
NadyKoskei <cherotichnady@gmail.com>
2026-02-16 15:59:05 +0300

    submit event on form

--- src/js/exe.js
+++ src/js/exe.js
@@ -8,3 +8,14 @@ var nameInput = document.getElementById("name");
 var yearInput = document.getElementById("year");
 var resultDiv = document.getElementById("result");
 
+
+//adding a submit event listener to the form element that will trigger a function when the form is submitted. The function will prevent the default behavior of the form submission, which is to reload the page, and then it will collect the values of the name and year input fields, trim any whitespace from the name, and convert the year to a number. It will also get the current year using the Date object.
+userForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var name = nameInput.value.trim();
+  var year = Number(yearInput.value);
+  var currentYear = new Date().getFullYear(); // this will get the current year using the Date object and the getFullYear method, which returns the year of the specified date according to local time.
+});
+
+
