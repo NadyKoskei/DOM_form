79c1b5d18094b158f6787cb8d133ff1836549041
NadyKoskei <cherotichnady@gmail.com>
2026-02-17 08:39:22 +0300

    age, leap or not, even or odd

--- src/js/exe.js
+++ src/js/exe.js
@@ -1,4 +1,4 @@
-// this is a program that will collect an users name and the year they were born. then it will calculate 
+// this is a program that will collect an users name and the year they were born. then it will calculate
 // 1) how old they are 2) leap year or not 3) the name of theit generation 4)even or odd year of birth
 //and give them a message with all the information when they click the submit button
 //also if the year submitted is in the future, it will give them a message saying that they are not born yet, and the year of birth is invalid. Also the year must be a number and not a string, if the user submits a string instead of a number, it will give them a message saying that the year of birth is invalid. Also the name must not be empty, if the user submits an empty name, it will give them a message saying that the name is invalid.
@@ -7,7 +7,6 @@ var userForm = document.getElementById("userForm");
 var nameInput = document.getElementById("name");
 var yearInput = document.getElementById("year");
 var resultDiv = document.getElementById("result");
-
 
 //adding a submit event listener to the form element that will trigger a function when the form is submitted. The function will prevent the default behavior of the form submission, which is to reload the page, and then it will collect the values of the name and year input fields, trim any whitespace from the name, and convert the year to a number. It will also get the current year using the Date object.
 userForm.addEventListener("submit", function (event) {
@@ -31,17 +30,23 @@ userForm.addEventListener("submit", function (event) {
     return;
   }
 
-//checking if the year is in the future
+  //checking if the year is in the future
   if (year > currentYear) {
     resultDiv.innerHTML =
       "You are not born yet. Please enter a valid year of birth.";
     return;
   }
 
+  // this will calculate the age of the user by subtracting the year of birth from the current year
+  var age = currentYear - year;
+
+  // this will check if the year is a leap year by checking if it is divisible by 4 and not divisible by 100, or if it is divisible by 400. It will then set the leapYear variable to "a leap year" or "not a leap year" accordingly.
+  var leapYear =
+    (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
+      ? "a leap year"
+      : "not a leap year";
+
+  //even or odd year of birth
+  var evenOdd = year % 2 === 0 ? "even" : "odd";
   
 });
-
-
-
-
-
